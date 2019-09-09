f3054bfeeec22dfdebce7a210b9a30d5c279d1d2
NotThatJonas <mr.joshuajonas@gmail.com>
2019-09-08 20:20:20 -0700

    setting up html page, initial js scripts and css

---
+++ assets/js/topics.js
@@ -0,0 +1,4 @@
+      // Initial array of animas for Giphys
+      var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+
+      var initialArray = ["kitten", "puppy", "sea otter", "pufferfish", "anteater", "red panda", "sloth", "darth", "falcon", "spider", "snake" ];
